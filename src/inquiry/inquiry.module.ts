8451817a4bfd5b070defda925404242111ac1529
prkskrs <prkskrs@gmail.com>
2023-05-11 09:50:18 +0530

    cron jobs

--- src/inquiry/inquiry.module.ts
+++ src/inquiry/inquiry.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from 'src/posts/posts.entity';
+import { PostsModule } from 'src/posts/posts.module';
+import { PostsService } from 'src/posts/posts.service';
 import { InquiryController } from './inquiry.controller';
 import { Inquiry } from './inquiry.entity';
 import { InquiryService } from './inquiry.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Inquiry, Post])],
+  imports: [TypeOrmModule.forFeature([Inquiry, Post]),PostsModule],
   controllers: [InquiryController],
-  providers: [InquiryService]
+  providers: [InquiryService,PostsService]
 })
 export class InquiryModule { }
